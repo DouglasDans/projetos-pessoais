7f30920e5e65d171999871de3e62d73ceb3ab5da
Douglas Nascimento <douglas.dans@gmail.com>
2022-04-05 09:55:44 -0300

    Tenis-Page Added

---
+++ Tenis-Page/script.js
@@ -0,0 +1,54 @@
+// MENU MOBILE
+
+let toggleButton = document.querySelector(".mobile-dropdown");
+let mobileNav = document.querySelector(".mobile-nav");
+
+toggleButton.addEventListener("click", function (){
+    mobileNav.classList.toggle("mobile-nav-active")
+})
+
+//SLIDER
+
+function ThumbnailPlugin(main) {
+    return (slider) => {
+      function removeActive() {
+        slider.slides.forEach((slide) => {
+          slide.classList.remove("active")
+        })
+      }
+      function addActive(idx) {
+        slider.slides[idx].classList.add("active")
+      }
+
+      function addClickEvents() {
+        slider.slides.forEach((slide, idx) => {
+          slide.addEventListener("click", () => {
+            main.moveToIdx(idx)
+          })
+        })
+      }
+
+      slider.on("created", () => {
+        addActive(slider.track.details.rel)
+        addClickEvents()
+        main.on("animationStarted", (main) => {
+          removeActive()
+          const next = main.animator.targetIdx || 0
+          addActive(main.track.absToRel(next))
+          slider.moveToIdx(next)
+        })
+      })
+    }
+  }
+  var slider = new KeenSlider("#my-keen-slider")
+  var thumbnails = new KeenSlider(
+    "#thumbnails",
+    {
+      initial: 0,
+      slides: {
+        perView: 3,
+        spacing: 10,
+      },
+    },
+    [ThumbnailPlugin(this.slider)]
+  )
